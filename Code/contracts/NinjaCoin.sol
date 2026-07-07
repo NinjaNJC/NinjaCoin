4913fd100225414b04a650671956d8eeaf73596b
NinjaCreator <rahim28377@gmail.com>
2026-07-07 15:15:17 +0300

    Clean project and keep NinjaCoin tests

--- Code/contracts/NinjaCoin.sol
+++ Code/contracts/NinjaCoin.sol
@@ -3,11 +3,22 @@
 pragma solidity ^0.8.28;
 
 contract NinjaCoin {
+
     string public name = "NinjaCoin";
     string public symbol = "NJC";
 
     uint256 public totalSupply = 210000000;
 
+    mapping(address => uint256) public balances;
+
     constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Not enough NJC");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
     }
 }
---
+++ Code/test/NinjaCoin.ts
@@ -0,0 +1,35 @@
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("NinjaCoin", function () {
+
+  it("Should give total supply to the creator", async function () {
+    const { ethers } = await hre.network.connect();
+
+    const [owner] = await ethers.getSigners();
+
+    const NinjaCoin = await ethers.getContractFactory("NinjaCoin");
+    const coin = await NinjaCoin.deploy();
+
+    const balance = await coin.balances(owner.address);
+
+    expect(balance).to.equal(210000000);
+  });
+
+
+  it("Should transfer NJC between users", async function () {
+    const { ethers } = await hre.network.connect();
+
+    const [owner, user] = await ethers.getSigners();
+
+    const NinjaCoin = await ethers.getContractFactory("NinjaCoin");
+    const coin = await NinjaCoin.deploy();
+
+    await coin.transfer(user.address, 1000);
+
+    const userBalance = await coin.balances(user.address);
+
+    expect(userBalance).to.equal(1000);
+  });
+
+});
