1f4069a45aa26a19b3669646a310a307db9e3a78
NinjaCreator <rahim28377@gmail.com>
2026-07-07 14:54:45 +0300

    Create first NinjaCoin smart contract

---
+++ Code/contracts/NinjaCoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract NinjaCoin {
+    string public name = "NinjaCoin";
+    string public symbol = "NJC";
+
+    uint256 public totalSupply = 210000000;
+
+    constructor() {
+    }
+}
