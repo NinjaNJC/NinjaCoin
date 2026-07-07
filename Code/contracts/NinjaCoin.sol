016d49c3c29c2f1c5d125ff2df3906b1b61d6616
NinjaCreator <rahim28377@gmail.com>
2026-07-07 15:40:27 +0300

    Upgrade NinjCoin to ERC20 style

--- Code/contracts/NinjaCoin.sol
+++ Code/contracts/NinjaCoin.sol
@@ -11,11 +11,19 @@ contract NinjaCoin {
 
     uint256 public totalSupply = 210000000 * 10 ** decimals;
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) private balances;
+
+    mapping(address => mapping(address => uint256)) private allowances;
 
     event Transfer(
         address indexed from,
         address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
         uint256 value
     );
 
@@ -31,7 +39,12 @@ contract NinjaCoin {
     }
 
 
-    function transfer(address to, uint256 amount) public {
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+
+    function transfer(address to, uint256 amount) public returns (bool) {
 
         require(to != address(0), "Invalid address");
 
@@ -40,16 +53,69 @@ contract NinjaCoin {
             "Not enough NJC"
         );
 
-
         balances[msg.sender] -= amount;
-
         balances[to] += amount;
-
 
         emit Transfer(
             msg.sender,
             to,
             amount
         );
+
+        return true;
+    }
+
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+
+        allowances[msg.sender][spender] = amount;
+
+        emit Approval(
+            msg.sender,
+            spender,
+            amount
+        );
+
+        return true;
+    }
+
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+
+        return allowances[owner][spender];
+
+    }
+
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public returns (bool) {
+
+        require(
+            balances[from] >= amount,
+            "Not enough NJC"
+        );
+
+        require(
+            allowances[from][msg.sender] >= amount,
+            "Not approved"
+        );
+
+
+        balances[from] -= amount;
+        balances[to] += amount;
+
+        allowances[from][msg.sender] -= amount;
+
+
+        emit Transfer(
+            from,
+            to,
+            amount
+        );
+
+        return true;
     }
 }
--- Code/test/NinjaCoin.ts
+++ Code/test/NinjaCoin.ts
@@ -4,6 +4,7 @@ import hre from "hardhat";
 describe("NinjaCoin", function () {
 
   it("Should give total supply to the creator", async function () {
+
     const { ethers } = await hre.network.connect();
 
     const [owner] = await ethers.getSigners();
@@ -11,13 +12,15 @@ describe("NinjaCoin", function () {
     const NinjaCoin = await ethers.getContractFactory("NinjaCoin");
     const coin = await NinjaCoin.deploy();
 
-    const balance = await coin.balances(owner.address);
+    const balance = await coin.balanceOf(owner.address);
 
     expect(balance).to.equal(210000000n * 10n ** 18n);
+
   });
 
 
   it("Should transfer NJC between users", async function () {
+
     const { ethers } = await hre.network.connect();
 
     const [owner, user] = await ethers.getSigners();
@@ -25,11 +28,15 @@ describe("NinjaCoin", function () {
     const NinjaCoin = await ethers.getContractFactory("NinjaCoin");
     const coin = await NinjaCoin.deploy();
 
+
     await coin.transfer(user.address, 1000);
 
-    const userBalance = await coin.balances(user.address);
+
+    const userBalance = await coin.balanceOf(user.address);
+
 
     expect(userBalance).to.equal(1000);
+
   });
 
 });
